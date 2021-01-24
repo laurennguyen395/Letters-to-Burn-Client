c783095a25f3d823a35d5d14104a103699cd55bc
laurennguyen395 <laurennguyen395@gmail.com>
2021-01-23 16:38:52 -0800

    small styling on login and register form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -33,38 +33,40 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="login">
-        <h4>Let it all out</h4>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              onChange={this.handleChange}
-              type="email"
-              id="email"
-              name="email"
-              value={this.state.email}
-            />
-          </div>
+      <div className="hold">
+        <div className="container">
+          <h4>Let It Burn</h4>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-control">
+              <input
+                onChange={this.handleChange}
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                value={this.state.email}
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={this.handleChange}
-              type="password"
-              id="password"
-              name="password"
-              value={this.state.password}
-            />
-          </div>
-          <button type="submit" className="login-btn">
-            Login
-          </button>
-        </form>
+            <div className="form-control">
+              <input
+                onChange={this.handleChange}
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={this.state.password}
+              />
+            </div>
+            <button type="submit" className="login-btn">
+              Login
+            </button>
 
-        <a class="register-link" href="register">
-          <p className="regP">Register</p>
-        </a>
+            <p class="text">
+              Don't have an account? <a href="Register">Register</a>
+            </p>
+          </form>
+        </div>
       </div>
     )
   }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -39,74 +39,70 @@ const Register = (props) => {
   }
 
   return (
-    <div className="RegDiv">
-      <h4 className="registerP">
-        Want to send a letter to your boss? An ex? A colleague? Scared that will
-        get you in trouble? Write it all out here..and then burn it.{' '}
-      </h4>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            className="RegInputBox"
-            onChange={handleFirstName}
-            value={firstName}
-            type="text"
-            id="firstName"
-            name="firstName"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            className="RegInputBox"
-            onChange={handleLastName}
-            value={lastName}
-            type="text"
-            id="lastName"
-            name="lastName"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="name">Email:</label>
-          <input
-            className="RegInputBox"
-            onChange={handleEmail}
-            value={email}
-            type="email"
-            id="email"
-            name="email"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="name">Password:</label>
-          <input
-            className="RegInputBox"
-            onChange={handlePassword}
-            value={password}
-            type="password"
-            id="password"
-            name="password"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirm-password">Confirm Password:</label>
-          <input
-            className="RegInputBox"
-            onChange={handleConfirmPassword}
-            value={confirmPassword}
-            type="password"
-            id="confirm-password"
-            name="confirm-password"
-            required
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+    <div className="hold">
+      <div className="container">
+        <h4>Register</h4>
+        <form onSubmit={handleSubmit} className="reg-form">
+          <div className="form-control">
+            <input
+              onChange={handleFirstName}
+              value={firstName}
+              type="text"
+              id="firstName"
+              name="firstName"
+              placeholder="First Name"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <input
+              onChange={handleLastName}
+              value={lastName}
+              type="text"
+              id="lastName"
+              name="lastName"
+              placeholder="Last Name"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <input
+              onChange={handleEmail}
+              value={email}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <input
+              onChange={handlePassword}
+              value={password}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <input
+              onChange={handleConfirmPassword}
+              value={confirmPassword}
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
+          <button type="submit" className="reg-btn">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
